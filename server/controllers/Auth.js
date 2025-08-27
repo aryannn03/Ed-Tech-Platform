a25dc44c7137efa7675509d775e1816f2d6717a0
aryan <124302386+aryannn03@users.noreply.github.com>
2025-08-28 02:27:33 +0530

    sign up added

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const OTP = require("../models/OTP");
 
 const otpGenerator = require("otp-generator");
-
+const bcrypt=require('bcrypt');
 // send OTP
 
 exports.sendOTP = async(req,res)=>{
@@ -58,9 +58,80 @@ exports.sendOTP = async(req,res)=>{
     }
 
 };
-// sendOtp
+
 
 // signup
+exports.signUp=async (req,res)=>{
+    
+    try{
+        // data from req body
+    const {firstName,lastName,email,password,accountType,contactNumber,confirmPassword,otp} = req.body;
+    if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
+        return res.status(400).json({
+            success:false,
+            message:'All fields are required'
+        })
+    }
+    if(password !== confirmPassword){
+        return res.status(400).json({
+            success:false,
+            message:'Password and Confirm Password do not match'
+        })
+    }
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(401).json({
+            success:false,
+            message:'User already registered'
+        })
+    }
+    const recentOTP = await OTP.find({email}).sort({createdAt:-1}).limit(1);
+    console.log(recentOTP);
+    if(recentOTP.length === 0){
+        return res.status(400).json({
+            success:false,
+            message:'OTP not found, please request for a new OTP'
+        })
+    }
+    if(recentOTP.otp !== otp){
+        return res.status(400).json({
+            success:false,
+            message:'Invalid OTP'
+        })
+    }
+    const profileDetails=await Profile.create({
+        gender:null,
+        dateOfBirth:null,
+        about:null,
+        contactNumber:null,
+    })
+// hashed password
+    const hasedPassword = await bcrypt.hash(password,10);
+    const user=await User.create({
+        firstName, 
+        lastName,
+        email,
+        password:hasedPassword,
+        accountType,
+        contactNumber,
+        additionalDetails:profileDetails._id,
+        image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
+    });
+    return res.status(200).json({
+        success:true,
+        message:"User registered successfully",
+        user,
+    })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"User cannot be registered. Please try again."
+        })
+        
+    }
+}
 
 // login
 
